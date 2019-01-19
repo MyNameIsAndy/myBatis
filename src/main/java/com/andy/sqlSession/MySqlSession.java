b0623fca2ebcdd8736c1173f4e0f131cfe2cd327
andu <chao1248640895>
2019-01-19 09:37:52 +0800

    添加注释

--- src/main/java/com/andy/sqlSession/MySqlSession.java
+++ src/main/java/com/andy/sqlSession/MySqlSession.java
@@ -21,7 +21,7 @@ public class MySqlSession {
     }
     @SuppressWarnings("unchecked")
     public <T> T getMapper(Class<T> clas){
-        //动态代理调用
+        //动态代理调用该方法用于为指定类装载器、一组接口及调用处理器生成动态代理类实例
         return (T) Proxy.newProxyInstance(clas.getClassLoader(),new Class[]{clas},
                 new MyMapperProxy(this,myConfiguration));
     }
